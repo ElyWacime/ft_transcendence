0daa8a5dda19a2e19778700fc2a09be2c3bf714f
skamroun <saad.kamroun@gmail.com>
2026-02-19 17:39:18 +0000

    WebSocket Closed ON logout

--- frontend/src/context/WebSocketContext.tsx
+++ frontend/src/context/WebSocketContext.tsx
@@ -1,15 +1,31 @@
 import { createContext, useContext, useEffect, useRef, useState } from "react";
+import { useAuth } from "./AuthContext";
 
 const WebSocketContext = createContext(null);
 
 export const WebSocketProvider = ({ children }) => {
+  const { isLoggedIn } = useAuth();
+
   const wsRef = useRef(null);
+  const [ws, setWs] = useState(null);
   const [isReady, setIsReady] = useState(false);
 
   useEffect(() => {
+
+    if (!isLoggedIn) {
+      if (wsRef.current) {
+        console.log("Closing WS because user logged out");
+        wsRef.current.close();
+        wsRef.current = null;
+        setWs(null);
+        setIsReady(false);
+      }
+      return;
+    }
     const socket = new WebSocket(`ws://${import.meta.env.VITE_DOMAIN}:3000/ws`);
 
     wsRef.current = socket;
+    setWs(socket);
 
     socket.onopen = () => {
       console.log("WebSocket connected");
@@ -21,19 +37,12 @@ export const WebSocketProvider = ({ children }) => {
       setIsReady(false);
     };
 
-    socket.onerror = (err) => {
-      console.error("WebSocket error:", err);
-    };
+    return () => socket.close();
 
-    return () => {
-      socket.close();
-    };
-  }, []);
+  }, [isLoggedIn]);
 
   return (
-    <WebSocketContext.Provider
-      value={{ ws: wsRef.current, isReady }}
-    >
+    <WebSocketContext.Provider value={{ ws, isReady }}>
       {children}
     </WebSocketContext.Provider>
   );
