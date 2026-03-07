8bb1f50012470a9e84da23a70b33c15f56809610
resh <redouancherrat@gmail.com>
2026-03-07 17:06:13 +0000

    socket middlware and validations schemas

--- frontend/src/context/ChatSocketContext.tsx
+++ frontend/src/context/ChatSocketContext.tsx
@@ -32,9 +32,7 @@ export const ChatSocketProvider = ({ children }: { children: ReactNode }) => {
     const [pendingInvitations, setPendingInvitations] = useState<any>({})
     const [blockedConversations, setBlockedConversations] = useState<any>({})
 
-
     const { isLoggedIn } = useAuth();
-
 
   useEffect(() => {
     if (!isLoggedIn) {
@@ -46,17 +44,17 @@ export const ChatSocketProvider = ({ children }: { children: ReactNode }) => {
     setSocket(chatSocket);
     chatSocket.on("connect", async () => {
         try {
-          const res = await fetchWithAuth(`${SERVER_URL}/api/chat/getCookieValue`, { method: 'GET', credentials: 'include' });
-          const usercookie = await res.json();
-          const userId = usercookie.user_id;
-          setCurrentUser({ id: userId });
-          chatSocket.emit('authenticate', userId);
+          chatSocket.emit('authenticate');
           setIsConnected(true);
         } catch (error) {
           console.error('Failed to authenticate:', error);
         }
       }
     );
+
+    chatSocket.on("authenticate", (data: any) => {
+      setCurrentUser({ id: data.userId });
+    });
 
     chatSocket.on("disconnect", () => {
       setFriendsList({});
@@ -93,7 +91,7 @@ export const ChatSocketProvider = ({ children }: { children: ReactNode }) => {
       }));
     });
 
-    const handleGameInviteResponse = (data: any) => {
+    const handleInviteResponse = (data: any) => {
       if (data.invitationType === "game_request") {
         if (data.accepted) {
           navigate(`/loading?mode=2`);
@@ -114,9 +112,9 @@ export const ChatSocketProvider = ({ children }: { children: ReactNode }) => {
         }
       }))
     }    
-    chatSocket.on('InviteResponse', handleGameInviteResponse)
+    chatSocket.on('InviteResponse', handleInviteResponse)
 
-    const handleCancelInvite = async ({invitationType}) => {
+    const handleCancelInvite = async () => {
       setInvitePrompt(null);
     }
     chatSocket.on("cancelInvite", handleCancelInvite);
@@ -157,7 +155,7 @@ export const ChatSocketProvider = ({ children }: { children: ReactNode }) => {
     return () => {
       chatSocket.disconnect();
       chatSocket.off('unfriend', handleUnfriend)
-      chatSocket.off('InviteResponse', handleGameInviteResponse)
+      chatSocket.off('InviteResponse', handleInviteResponse)
       chatSocket.off('blockStatusChanged', handleBlockStatusChanged)
 
     };
